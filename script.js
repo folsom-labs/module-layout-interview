7de57fe4a7d0c7ede6dd20740216fb4c377b6888
Alex Liotta <aliotta343@gmail.com>
2015-12-05 17:43:31 -0800

    deleted unnecessary code

--- script.js
+++ script.js
@@ -117,21 +117,6 @@ function initialize() {
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions),
         button = $('#drawModules');
 
-        var rectangle = new google.maps.Rectangle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            bounds: {
-                north: 33.685,
-                south: 33.671,
-                east: -116.234,
-                west: -116.251
-            }
-        });
-
     button.on('click', function () {
         button.prop('disabled', true);
         getPolygon(map, function(polygon){
